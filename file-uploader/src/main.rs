62275eff60069bdb325d55fec9ef6858ec20a862
micheal-ndoh <michaelndoh9@gmail.com>
2025-03-19 14:53:55 +0100

    updated

--- file-uploader/src/main.rs
+++ file-uploader/src/main.rs
@@ -56,33 +56,3 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-// use structopt::StructOpt;
-// use std::path::{PathBuf, Path};
-// use std::fs::{self, File};
-// use std::io::{Read, Write};
-
-// #[derive(Debug, StructOpt)]
-// struct Opts {
-//     #[structopt(parse(from_os_str))]
-//     file: PathBuf,
-// }
-
-// fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let opts = Opts::from_args();
-
-//     let mut file = File::open(&opts.file)?;
-//     let mut contents = Vec::new();
-//     file.read_to_end(&mut contents)?;
-
-//     let destination_folder = Path::new("./uploads");
-//     fs::create_dir_all(destination_folder)?;
-
-//     let destination_file = destination_folder.join(opts.file.file_name().unwrap());
-
-//     let mut dest_file = File::create(destination_file)?;
-//     dest_file.write_all(&contents)?;
-
-//     println!("File uploaded successfully.");
-
-//     Ok(())
-// }
