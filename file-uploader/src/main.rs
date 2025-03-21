ee4ff1ef563f1dbe4481291bfe490fdc01376825
micheal-ndoh <michaelndoh9@gmail.com>
2025-03-21 08:29:46 +0100

    Update: modifiied to accept multiples uploaded  files

--- file-uploader/src/main.rs
+++ file-uploader/src/main.rs
@@ -7,52 +7,44 @@ use tokio::io::AsyncReadExt;
 #[derive(Debug, StructOpt)]
 pub struct Opt {
     #[structopt(parse(from_os_str))]
-    infile: PathBuf,
-
-    #[structopt(short, long, parse(from_os_str))]
-    outfile: Option<PathBuf>,
+    infile: Vec<PathBuf>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts = Opt::from_args();
 
-    let mut file = File::open(&opts.infile).await?;
-    let mut file_data = Vec::new();
-    file.read_to_end(&mut file_data).await?;
+    for el in opts.infile {
+        let mut file = File::open(&el).await?;
+        let mut file_data = Vec::new();
+        file.read_to_end(&mut file_data).await?;
+        let file_name = el.file_name().unwrap().to_string_lossy().to_string();
 
-    let file_name = opts
-        .infile
-        .file_name()
-        .unwrap()
-        .to_string_lossy()
-        .to_string();
+        let form = multipart::Form::new().part(
+            "fileupload",
+            multipart::Part::bytes(file_data).file_name(file_name),
+        );
 
-    let form = multipart::Form::new().part(
-        "fileupload",
-        multipart::Part::bytes(file_data).file_name(file_name),
-    );
+        let client = reqwest::Client::new();
+        let response = match client
+            .post("http://localhost:3000/index")
+            .multipart(form)
+            .send()
+            .await
+        {
+            Ok(response) => response,
+            Err(e) => {
+                eprintln!("Failed to connect to server: {}", e);
+                return Ok(());
+            }
+        };
 
-    let client = reqwest::Client::new();
-    let response = match client
-        .post("http://localhost:3000/index")
-        .multipart(form)
-        .send()
-        .await
-    {
-        Ok(response) => response,
-        Err(e) => {
-            eprintln!("Failed to connect to server: {}", e);
-            return Ok(());
+        if response.status().is_success() {
+            println!("File uploaded successfully!");
+        } else {
+            println!("Failed to upload file: {}", response.status());
+            break;
         }
-    };
-
-    if response.status().is_success() {
-        println!("File uploaded successfully!");
-    } else {
-        println!("Failed to upload file: {}", response.status());
     }
-
     Ok(())
 }
-
