22bcc44db2d2c6a3b83ddbe99b69fcc2d51eaa9a
micheal-ndoh <michaelndoh9@gmail.com>
2025-03-30 13:40:11 +0100

    feat: integrate database connection into upload and compression services

--- upload-endpoint/src/main.rs
+++ upload-endpoint/src/main.rs
@@ -1,35 +1,52 @@
 mod handlers;
+mod db;
+
 use axum::{
     extract::{Path, Query},
     response::IntoResponse,
     routing::{get, post},
-    Router,
+    Router, Extension,
 };
 use handlers::{compress_file, upload_file};
 use serde::Deserialize;
 use tokio::net::TcpListener;
 use tower_http::{services::ServeDir, trace::TraceLayer};
+use db::establish_connection;
+
+use dotenvy::dotenv;
+use std::env;
+
 
 #[tokio::main]
 async fn main() {
+    // dotenv().ok(); 
+
+    // Initialize database connection
+    let pool = establish_connection().await;
+
+    // Compression service routes
     let compressed = Router::new()
         .route("/compress", post(compress_file::compress_all_files))
-        .nest_service("/files", ServeDir::new("compressed"));
+        .nest_service("/files", ServeDir::new("compressed"))
+        .layer(Extension(pool.clone())); // Pass DB connection
 
+    // Upload service routes
     let uploads = Router::new()
         .route("/upload", post(upload_file::upload_files))
-        .nest_service("/files", ServeDir::new("uploads"));
+        .nest_service("/files", ServeDir::new("uploads"))
+        .layer(Extension(pool.clone())); // Pass DB connection
 
-    // Create a new Axum router
+    // Main API router
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/path-examples/:parameter", get(path_example_handler))
         .nest("/uploader", uploads)
         .nest("/compressor", compressed)
         .fallback(|| async { r#"{"status":404,"message":"Resource Not Found"}"# })
-        .layer(TraceLayer::new_for_http());
+        .layer(TraceLayer::new_for_http())
+        .layer(Extension(pool)); // Make DB accessible to all routes
 
-    // Define the address for the server to listen on
+    // Define server address
     let ip_addr = "0.0.0.0:3000";
     let listener = match TcpListener::bind(ip_addr).await {
         Ok(listener) => listener,
@@ -38,6 +55,7 @@ async fn main() {
             return;
         }
     };
+
     println!("[INFO]: Application is running http://{ip_addr}");
 
     // Start the server
