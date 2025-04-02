71e269926badd4f051a30241e956ad3ee55574d3
micheal-ndoh <michaelndoh9@gmail.com>
2025-04-02 17:18:15 +0100

    feat: enhance server initialization with improved error handling and logging

--- upload-endpoint/src/main.rs
+++ upload-endpoint/src/main.rs
@@ -11,59 +11,52 @@ use db::establish_connection;
 use handlers::{compress_file, upload_file};
 use serde::Deserialize;
 use tokio::net::TcpListener;
+use std::net::SocketAddr;
 use tower_http::{services::ServeDir, trace::TraceLayer};
-
-use dotenvy::dotenv;
-// use std::env;
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
+    dotenvy::dotenv().ok();
 
     // Initialize database connection
-    let pool = establish_connection().await;
+    let pool = match establish_connection().await {
+        Ok(pool) => pool,
+        Err(e) => {
+            eprintln!("Failed to connect to database: {}", e);
+            return;
+        }
+    };
 
     // Compression service routes
-    let compressed = Router::new()
+    let compressor = Router::new()
         .route("/compress", post(compress_file::compress_all_files))
         .nest_service("/files", ServeDir::new("compressed"))
-        .layer(Extension(pool.clone())); // Pass DB connection
+        .layer(Extension(pool.clone()));
 
     // Upload service routes
     let uploads = Router::new()
         .route("/upload", post(upload_file::upload_files))
         .nest_service("/files", ServeDir::new("uploads"))
-        .layer(Extension(pool.clone())); // Pass DB connection
+        .layer(Extension(pool.clone()));
 
     // Main API router
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
         .route("/path-examples/:parameter", get(path_example_handler))
         .nest("/uploader", uploads)
-        .nest("/compressor", compressed)
+        .nest("/compressor", compressor)
         .fallback(|| async { r#"{"status":404,"message":"Resource Not Found"}"# })
         .layer(TraceLayer::new_for_http())
-        .layer(Extension(pool)); // Make DB accessible to all routes
+        .layer(Extension(pool));
 
-    // Define server address
-    let ip_addr = "0.0.0.0:3000";
-    let listener = match TcpListener::bind(ip_addr).await {
-        Ok(listener) => listener,
-        Err(e) => {
-            eprintln!("Failed to bind TcpListener to server {e}");
-            return;
-        }
-    };
-
-    println!("[INFO]: Application is running http://{ip_addr}");
-
-    // Start the server
-    if let Err(e) = axum::serve(listener, app).await {
-        eprintln!("{e}")
-    }
+        let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    // Start server
+    let addr = TcpListener::bind(addr).await.unwrap();
+    println!("Server running on http://{}", addr.local_addr().unwrap());
+    
+    axum::serve(addr, app.into_make_service());
 }
 
-#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 struct MyQuery {
     gis: Option<bool>,
