f42fd8977eb84025dcdc97c043ba5fe4076df93e
micheal-ndoh <michaelndoh9@gmail.com>
2025-04-02 09:32:49 +0100

    feat: enhance database connection function to load .env and provide fallback URL

--- upload-endpoint/src/db.rs
+++ upload-endpoint/src/db.rs
@@ -1,11 +1,26 @@
+// upload-endpoint/src/db.rs
 use sqlx::{PgPool, postgres::PgPoolOptions};
-use std::env;
+use dotenvy::dotenv;
 
-pub async fn establish_connection() -> PgPool {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgPoolOptions::new()
+pub async fn establish_connection() -> Result<PgPool, String> {
+    dotenv().ok();
+    dotenvy::from_path("upload-endpoint/.env").ok();
+   
+      // Fallback URL if .env loading fails
+      let database_url = std::env::var("DATABASE_URL")
+      .unwrap_or_else(|_| "postgres://micheal:password@10.38.229.35:5432/compression_tasks?sslmode=disable".to_string());
+
+    
+    let pool = PgPoolOptions::new()
         .max_connections(5)
         .connect(&database_url)
         .await
-        .expect("Failed to connect to database")
+        .map_err(|e| format!("Failed to connect to database: {}", e))?;
+    
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .map_err(|e| format!("Failed to run migrations: {}", e))?;
+    
+    Ok(pool)
 }
