95eb3f9eb9a02f6451e6f0309a5354cb5201994d
micheal-ndoh <michaelndoh9@gmail.com>
2025-04-02 09:33:22 +0100

    refactor: remove obsolete files and queries related to compression_tasks

--- migrations/20250330104008_create_compression_tasks.sql
+++
@@ -1,7 +0,0 @@
--- Add migration script here
-
-CREATE TABLE compression_tasks (
-    id SERIAL PRIMARY KEY,
-    file_name TEXT NOT NULL,
-    status TEXT NOT NULL CHECK (status IN ('pending', 'completed', 'failed'))
-);
