41c91b1d4b84518ef71ffc96bdbd9e6888f9c31a
micheal-ndoh <michaelndoh9@gmail.com>
2025-04-02 17:18:29 +0100

    feat: simplify migration scripts by removing unnecessary index creation and drop statements

--- upload-endpoint/migrations/20250401220502_init.down.sql
+++ upload-endpoint/migrations/20250401220502_init.down.sql
@@ -1,5 +1,2 @@
 -- Add up migration script here
-
--- Completely undo the migration
-DROP INDEX IF EXISTS idx_compression_tasks_status;
-DROP TABLE IF EXISTS compression_tasks;
+DROP TABLE compression_tasks;
--- upload-endpoint/migrations/20250401220502_init.up.sql
+++ upload-endpoint/migrations/20250401220502_init.up.sql
@@ -1,7 +1,6 @@
+
 CREATE TABLE compression_tasks (
     id SERIAL PRIMARY KEY,
     file_name TEXT NOT NULL,
     status TEXT NOT NULL DEFAULT 'pending'
 );
-
-CREATE INDEX idx_compression_tasks_status ON compression_tasks(status);
